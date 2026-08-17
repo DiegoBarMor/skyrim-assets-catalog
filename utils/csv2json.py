27fdfdafc17e5b58aa4d41f327c3e3da23f978b5
DBM <diegobarmor42@gmail.com>
2026-08-17 17:32:47 +0200

    reworking the csv2json preprocessing

--- utils/csv2json.py
+++ utils/csv2json.py
@@ -1,20 +1,72 @@
 import sys
+import json
 import pandas as pd
 from pathlib import Path
 
 # ------------------------------------------------------------------------------
 def preprocess(df: pd.DataFrame) -> pd.DataFrame:
     df = df.dropna()
-    df["parent"] = df["pseudo_path"].apply(lambda p: str(Path(p).parent))
-    df["depth"] = df["pseudo_path"].str.count("/")
+    df = df.drop_duplicates("name_nif")
+
+    df["directories"] = df["pseudo_path"]\
+        .apply(lambda p: str(Path(p).parent))
+    df["name_nif"] = df["name_nif"].apply(lambda s: f"{s}.nif")
+
     df = df.drop(columns = ["path_texture", "pseudo_path"])
-    df = df.sort_values(by = ["parent"])
+    df = df.sort_values(by = ["directories"])
     return df
+
+
+# ------------------------------------------------------------------------------
+def init_dir_tree(unique_dirs: list[str]) -> dict[str, dict]:
+    tree: dict[str, dict] = {}
+    for d in unique_dirs:
+        if d == '.': continue # skip root
+        parts = d.split('/')
+        current = tree
+        for part in parts:
+            if part not in current:
+                current[part] = {}
+            current = current[part]
+
+    return tree
+
+
+# ------------------------------------------------------------------------------
+def populate_dir_tree(tree: dict[str, dict], data: list[list[str]]) -> None:
+    def get_inner_dict(path: str) -> dict[str, dict]:
+        if path == '.': return tree
+        parts = path.split('/')
+        current = tree
+        for part in parts: current = current[part]
+        return current
+
+    for entry in data:
+        ### ASSUME HARDCODED COLUMNS
+        editor_name, base_form, name_nif, directories = entry
+        node = get_inner_dict(directories)
+        if name_nif in node:
+            raise ValueError(f"Duplicate entry for '{name_nif}' in '{directories}'")
+
+        node[name_nif] = [editor_name, base_form]
+
+
+# ------------------------------------------------------------------------------
+def extract_tree(df: pd.DataFrame) -> dict[str, list|dict]:
+    json_flat = json.loads(df.to_json(orient = "split", index = False))
+    tree = init_dir_tree(df["directories"].unique())
+    populate_dir_tree(tree, json_flat["data"])
+    return {
+        "columns" : ["editor_name", "base_form"], # ASSUME HARDCODED COLUMNS
+        "data": tree
+    }
+
 
 # ------------------------------------------------------------------------------
 def main():
     df = preprocess(pd.read_csv(PATH_CSV))
-    df.to_json(PATH_JSON, orient = "split", index = False)
+    data = extract_tree(df)
+    PATH_JSON.write_text(json.dumps(data))
 
 
 ################################################################################
