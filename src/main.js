e4dcab4bca3e0c5135987200ea429a8df324a2f7
DBM <diegobarmor42@gmail.com>
2026-08-20 13:39:03 +0200

    thumbnails are now displayed next to the nif nodes

--- src/main.js
+++ src/main.js
@@ -17,12 +17,40 @@ function split_keys(node) {
 }
 
 /**
+ * @param {string} name_dir
+ */
+function get_html_dir(name_dir) {
+    return `<a href="#"><b>${name_dir}</b></a>`
+}
+
+/**
+ * @param {string} name_nif
+ */
+function get_html_nif(name_nif) {
+    url = URLS_THUMBNAILS[name_nif] || URL_MISSING_THUMBNAIL;
+    return `
+    <div class="nif-item">
+        <a href="#">${name_nif}</a>
+        <img src="${url}" alt="${name_nif}" onclick="wip_not_implemented('${url}')">
+    </div>
+    `
+}
+
+/**
+ * @param {string} url
+ */
+function wip_not_implemented(url) {
+    alert(`WIP: Not implemented yet. ${url}`);
+}
+
+/**
  * @param {SKNode} node
  */
 function update_node(node) {
     const { keys_dir, keys_nif } = split_keys(node);
-    document.getElementById("list_dirs").innerHTML = keys_dir.map((x) => `<a href="#">${x}</a>`).join("<br>");
-    document.getElementById("list_nifs").innerHTML = keys_nif.map((x) => `<a href="#">${x}</a>`).join("<br>");
+    document.getElementById("list_dirs").innerHTML = keys_dir.map(get_html_dir).join("<br>");
+    document.getElementById("list_nifs").innerHTML = keys_nif.map(get_html_nif).join("<br>");
 }
 
+var CURRENT_NODE = DATA_STATICS_META["data"];
 update_node(DATA_STATICS_META["data"]);
